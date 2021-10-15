7caec3c7b30d99cd8a9a98a2a72894290b8ef62e
GuilhermeJWT <guiromanno@gmail.com>
2021-10-14 22:15:13 -0300

    Criando o Service e enviando a Mensagem

--- src/main/java/br/com/systemsgs/rabbitmq/controller/EstoqueController.java
+++ src/main/java/br/com/systemsgs/rabbitmq/controller/EstoqueController.java
@@ -1,5 +1,6 @@
 package br.com.systemsgs.rabbitmq.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,14 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.systemsgs.rabbitmq.constantes.RabbitMQConstantes;
 import br.com.systemsgs.rabbitmq.dto.EstoqueDTO;
+import br.com.systemsgs.rabbitmq.service.RabbitMQService;
 
 @RestController
 @RequestMapping(value = "/api/estoque")
 public class EstoqueController {
 	
+	@Autowired
+	private RabbitMQService rabbitMqService;
+	
 	@PutMapping
 	public ResponseEntity alteraEstoque(@RequestBody EstoqueDTO estoqueDTO) {
+		this.rabbitMqService.enviaMensagem(RabbitMQConstantes.FILA_ESTOQUE, estoqueDTO);
+		
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
--- src/main/java/br/com/systemsgs/rabbitmq/controller/PrecoController.java
+++ src/main/java/br/com/systemsgs/rabbitmq/controller/PrecoController.java
@@ -1,5 +1,6 @@
 package br.com.systemsgs.rabbitmq.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,14 +8,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.systemsgs.rabbitmq.constantes.RabbitMQConstantes;
 import br.com.systemsgs.rabbitmq.dto.PrecoDTO;
+import br.com.systemsgs.rabbitmq.service.RabbitMQService;
 
 @RestController
 @RequestMapping(value = "/api/preco")
 public class PrecoController {
 	
+	@Autowired
+	private RabbitMQService rabbitMqService;
+	
 	@PutMapping
 	public ResponseEntity alteraPreco(@RequestBody PrecoDTO precoDTO) {
+		this.rabbitMqService.enviaMensagem(RabbitMQConstantes.FILA_PRECO, precoDTO);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
