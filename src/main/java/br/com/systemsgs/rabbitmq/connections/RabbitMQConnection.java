24e17c59b68b598d68be942f46617f6c376dc7f7
GuilhermeJWT <guiromanno@gmail.com>
2021-10-14 21:52:38 -0300

    Adiciona mais configurações na Classe

--- src/main/java/br/com/systemsgs/rabbitmq/connections/RabbitMQConnection.java
+++ src/main/java/br/com/systemsgs/rabbitmq/connections/RabbitMQConnection.java
@@ -1,14 +1,24 @@
 package br.com.systemsgs.rabbitmq.connections;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.stereotype.Component;
 
+import br.com.systemsgs.rabbitmq.constantes.RabbitMQConstantes;
+
 @Component
 public class RabbitMQConnection {
 	
 	private static final String NOME_EXCHANGE = "amq.direct";
+	private AmqpAdmin amqpAdmin;
+	
+	public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+		this.amqpAdmin = amqpAdmin;
+	}
 	
 	private Queue fila(String nomeFile) {
 		return new Queue(nomeFile, true, false, false);
@@ -19,7 +29,26 @@ public class RabbitMQConnection {
 	}
 	
 	private Binding relacionamento(Queue fila, DirectExchange troca) {
-		return new Binding(fila.getName(), Binding.DestinationType.EXCHANGE, troca.getName(), fila.getName(), null);
+		return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
+	}
+	
+	@PostConstruct
+	private void adiciona() {
+     Queue filaEstoque = this.fila(RabbitMQConstantes.FILA_ESTOQUE);
+     Queue filaPreco = this.fila(RabbitMQConstantes.FILA_PRECO);
+     
+     DirectExchange troca = this.trocaDireta();
+     
+     Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+     Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+     
+     this.amqpAdmin.declareQueue(filaEstoque);
+     this.amqpAdmin.declareQueue(filaPreco);
+     
+     this.amqpAdmin.declareExchange(troca);
+     
+     this.amqpAdmin.declareBinding(ligacaoEstoque);
+     this.amqpAdmin.declareBinding(ligacaoPreco);
 	}
 
 }
---
+++ src/main/java/br/com/systemsgs/rabbitmq/constantes/RabbitMQConstantes.java
@@ -0,0 +1,8 @@
+package br.com.systemsgs.rabbitmq.constantes;
+
+public class RabbitMQConstantes {
+	
+	public static final String FILA_ESTOQUE = "ESTOQUE";
+	public static final String FILA_PRECO = "PRECO";
+	
+}
